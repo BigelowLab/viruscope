220660626178eb25d330b8601761f8f59cc8b0b7
Joe Brown <brwnjm@gmail.com>
2015-07-01 10:50:22 -0400

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='viralscan',
-    version='0.1.3',
+    version='0.1.4',
     url='https://github.com/BigelowLab/viralscan',
     license='',
     author='Joe Brown',

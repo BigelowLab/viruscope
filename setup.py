358fe90cd088e7615f3c67ee70a5f9374bebd530
Joe Brown <brwnjm@gmail.com>
2016-03-09 20:01:39 -0800

    fixes version import, authors

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
+import io
+from os.path import join, dirname
 from setuptools import setup
 
 
-_locals = {}
-with open('viralscan.py') as fp:
-    exec(fp.read(), None, _locals)
-version = _locals['__version__']
+def get_version(relpath):
+  '''Read version info from a file without importing it'''
+  for line in io.open(join(dirname(__file__), relpath), encoding='cp437'):
+    if '__version__' in line:
+      if '"' in line:
+        # __version__ = "0.9"
+        return line.split('"')[1]
+      elif "'" in line:
+        return line.split("'")[1]
 
 setup(
     name='viralscan',
-    version=version,
+    version=get_version("viralscan.py"),
     url='https://github.com/BigelowLab/viralscan',
     license='',
-    author='Joe Brown', 'Ben Tupper'
+    author='Joe Brown, Ben Tupper',
     author_email='jmbrown@bigelow.org',
     description='',
     long_description=__doc__,

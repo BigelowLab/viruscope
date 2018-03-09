666a0e0c2c3e1ff4377f17ca94c40a0d790f6fe4
juliambrosman <julia@bigelow.org>
2018-03-09 10:49:53 -0500

    adds orf setup scripts from notebooks, begins organization

--- notebooks/phage_count_table.py
+++ notebooks/phage_count_table.py
@@ -18,9 +18,9 @@ def map_clstr_raw(clstr, singles=False):
         for cluster_start, group in itertools.groupby(fh, lambda l: l[0] == '>'):
             members = []
             rep_seq = ''
-            if not cluster_start: 
+            if not cluster_start:
                 for line in group:
-                    if "*" in line: 
+                    if "*" in line:
                         rep_seq = line.split(",")[1].split("...")[0].replace(">",'').replace(" ","")
                     else:
                         members.append(line.split(",")[1].split("...")[0].replace(">",'').replace(" ",""))
@@ -36,6 +36,96 @@ def map_clstr_raw(clstr, singles=False):
     return cluster_map
 
 
+def run_mica(fasta, out, db='/mnt/scgc/simon/databases/mica/nr-20150620-mica', num_alignments=10,
+           evalue=0.001,
+           threads=20, fields = ["qseqid", "sseqid", "pident", "length", "mismatch",
+                  "gapopen", "qstart", "qend", "sstart", "send", "evalue",
+                  "bitscore", "sallseqid", "score", "nident", "positive",
+                  "gaps", "ppos", "qframe", "sframe", "qseq", "sseq", "qlen",
+                  "slen", "salltitles"]):
+    cmd = ("mica-search --p='{threads}' --blastp 'blastp' {db} {query} "
+                   "--blast-args -outfmt '6 {fields}' "
+                   "-num_alignments {alignments} -evalue {evalue} -out {out}").format(db=db,
+                                                      query=fasta,
+                                                      fields=" ".join(fields),
+                                                      threads=threads,
+                                                      alignments=num_alignments,
+                                                      evalue=evalue,
+                                                      out=out)
+    print(cmd)
+    return cmd
+
+def run_blast(fasta, out, db='nr', num_alignments=10,
+           evalue=0.001,
+           threads=20, fields = ["qseqid", "sseqid", "pident", "length", "mismatch",
+                  "gapopen", "qstart", "qend", "sstart", "send", "evalue",
+                  "bitscore", "sallseqid", "score", "nident", "positive",
+                  "gaps", "ppos", "qframe", "sframe", "qseq", "sseq", "qlen",
+                  "slen", "salltitles"]):
+    cmd = ("blastp -db {db} -query {query} -outfmt "
+                   "'6 {fields}' "
+                   "-num_threads {threads} "
+                   "-num_alignments {alignments} "
+                   "-evalue {evalue} >> {out}").format(db=db,
+                                                      query=fasta,
+                                                      fields=" ".join(fields),
+                                                      threads=threads,
+                                                      alignments=num_alignments,
+                                                      evalue=evalue,
+                                                      out=out)
+    print(cmd)
+    return cmd
+
+def id_virus_orfs(micaout,
+                  keywords1 = 'phage,virus,prophage,terminase,t4-like,lambda-like,mu-like,capsid,tail,fiber,lambdoid,portal,tail,virion,lysis,podovirus,podo-like,head,baseplate,myovirus,siphovirus,structural',
+                  keywords2 = 'integrase,transposase'):
+    ''' takes mica output and returns dataframe with 1 if hits had viral signal, 0 if not
+    Args:
+        micaout (path): path to mica output file
+        keywords1 (string): comma separated list of keywords to search for to confirm that orf is viral
+        keywords2 (string): comma separated list of secondary keywords to search for to confirm that orf is viral
+    Returns:
+        pandas DataFrame of ORF classifications, 1 if viral, 0 if not (p1 for keywords1, p2 for kewords2)
+
+    '''
+    keywords1 = keywords1.split(",")
+    keywords2 = keywords2.split(",")
+
+    def search_phrases(string, keylist):
+        for i, k in enumerate(keylist):
+            if k in string:
+                return True
+            else:
+                if i == len(keylist)-1:
+                    return False
+                else:
+                    continue
+    cnames = "qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore sallseqid score nident positive gaps ppos qframe sframe qseq sseq qlen slen salltitles".split()
+    df = pd.read_csv(micaout, sep="\t", names=cnames)
+
+    p1_true = df[[search_phrases(i, keywords1) for i in df['salltitles']]]['qseqid'].unique()
+    p2_true = df[[search_phrases(i, keywords2) for i in df['salltitles']]]['qseqid'].unique()
+    qseqs = df['qseqid'].unique()
+
+    newdf = pd.DataFrame(data = {'orf':qseqs,
+                                'p1':[1 if q in p1_true else 0 for q in list(qseqs)],
+                                'p2':[1 if q in p2_true else 0 for q in list(qseqs)]})
+    return newdf
+
+def orf_map_fa(fasta):
+    names = []
+    contigs = []
+    lens = []
+
+    for name, seq in readfa(open(fasta)):
+        i = name.split(" ")[0]
+        names.append(i)
+        contig = "_".join(i.split("_")[:-1])
+        contigs.append(contig)
+        lens.append(len(seq) * 3)
+
+    return pd.DataFrame(data={'contig':contigs, 'id':names, 'len':lens})
+
 
 def phage_contig_table(clstr_map, prot_fasta, phage_hits_df, outfile=None):
     ''' create a summary of phage hits to contig orfs, mapping back cd-hit cluster seeds to orfs from individual genomes
@@ -45,7 +135,7 @@ def phage_contig_table(clstr_map, prot_fasta, phage_hits_df, outfile=None):
         phage_hits_df (pandas.DataFrame): phage hits dataframe from summary of all Mica results
         outfile (path): where to write output table, if None, none written
     Returns:
-        pandas dataframe 
+        pandas dataframe
     '''
     cm = clstr_map
     om = orf_map_fa(prot_fasta)
@@ -60,4 +150,3 @@ def phage_contig_table(clstr_map, prot_fasta, phage_hits_df, outfile=None):
         csum.to_csv(outfile)
 
     return csum
-
---
+++ viruscope/__init__.py
@@ -0,0 +1,3 @@
+from .tools import (file_transaction, remove_tmpdirs, remove_files,
+    safe_makedir, file_exists, check_dependencies, name_from_path, readfa,
+    format_fasta_record, write_fa_record
